ef1452bcf7ce25d08d7cac020b421a3895bd5cd5
w1-boudewijnrutger <rutger.boudewijn@edu-kw1c.nl>
2019-02-07 12:36:52 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,7 +1,15 @@
+<!DOCTYPE html>
 <html>
-    Hello world
-
-
-
-
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Brouwwereld | Uw bier specialist</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="Keywords" content="HTML, CSS, Javascript, jQuery">
+    <link rel="stylesheet" type="text/css" media="screen" href="style.css">
+    <script src="default.js"></script>
+</head>
+<body>
+    
+</body>
 </html>
