bfd77375ccd76671281ab02925b7e69cfc3161ec
w1-boudewijnrutger <rutger.boudewijn@edu-kw1c.nl>
2019-02-13 16:05:54 +0100

    header scroll effect

--- js/default.js
+++ js/default.js
@@ -1,3 +1,16 @@
-$('.click-me').click(function(){
-    $(this).css('display', 'none');
+var prevScrollpos = window.pageYOffset;
+$(document).scroll(function() {
+var currentScrollPos = window.pageYOffset;
+if(currentScrollPos > 100){
+ if (prevScrollpos  > currentScrollPos) {
+     document.getElementById("zone-header-wrapper").style.top = "0px";
+   
+ } else if($(window).width() >= 767){
+   document.getElementById("zone-header-wrapper").style.top = "-350px";
+ } else if($(window).width() <= 380 ){
+ 
+document.getElementById("zone-header-wrapper").style.top = "-100px";
+ }
+ prevScrollpos = currentScrollPos;
+}
 });
